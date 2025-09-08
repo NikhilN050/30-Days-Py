ba46094a00ddfd506d6160ec0c587f3a6c41e27d
NikhilN050 <nagarnikhil050gmail.com>
2025-09-08 21:05:38 +0530

    just revision

---
+++ revision_day1.py
@@ -0,0 +1,80 @@
+# Task 1
+print("Nikhil,20,codeing")
+
+# Task 2
+"""
+a = 20
+b = 2.0
+c = "nikhil"
+
+A = type(a)
+B = type(b)
+C = type(c)
+print(A,B,C)
+"""
+# Task 3
+'''
+x = 20
+y = 40
+print(x,y)
+y , x = x ,y
+print(x,y)
+'''
+# Task 4
+"""
+user = (input("Enter your Name :"))
+user2 = (input("Enter your age :"))
+print(f"Hello MY name is {user},my age is {user2}")
+"""
+# Task 5 
+"""
+user = int(input("Enter a number : "))
+user2 = int(input("Enter a number:"))
+sum = print(user + user2)
+diff = user - user2
+print(diff)
+product = print(user * user2)
+division = print(user / user2)
+reminder = print(user % user2)
+"""
+# Task 6 
+'''
+abc = 10+10+10/3
+print(abc)
+'''
+# Task 7 
+'''
+x = 10
+y = 20
+z = 30
+print(x , y , z)
+
+omg = z , y , x = x , z , x
+print(omg)
+'''
+# Task 8
+'''
+C = int(input("Enter a celius number :"))
+F = (C * 9/5) + 32
+print(F)
+'''
+# Task 9
+'''
+user = int(input("Enter a number :"))
+sqr = user ** 2
+cube = user ** 3
+print(f"This is a sqr {sqr} and this is cube {cube}")
+'''
+# Task 10
+'''
+user = input("Enter anything :")
+try:
+    val =int(user)
+    print("This is int")
+except ValueError:
+    try:
+        val =float(user)
+        print("float")
+    except ValueError:
+        print("value error")
+'''
