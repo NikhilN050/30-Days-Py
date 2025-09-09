2c808a79162a1f2362aa66ea22e6d33c04b25fd7
NikhilN050 <nagarnikhil050gmail.com>
2025-09-09 21:59:15 +0530

    next day

---
+++ day3.py
@@ -0,0 +1,14 @@
+# Task 1 – Print numbers 1 to 10
+'''
+for i in range(1,11):
+    print(i)
+'''
+# Task 2 – Print even numbers (1–20)
+'''
+i = 1
+while i <= 20:
+    if i % 2 ==0:
+        print(i)
+    i+=1
+'''
+# Task 3 – Multiplication Table
