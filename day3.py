4091082a57b44c289d2610c26e8cd46b379f39a3
NikhilN050 <nagarnikhil050gmail.com>
2025-09-10 20:20:31 +0530

    working on day 3 strong the loop and fun base

--- day3.py
+++ day3.py
@@ -12,3 +12,59 @@ while i <= 20:
     i+=1
 '''
 # Task 3 – Multiplication Table
+"""
+n = int(input("Enter a number :"))
+for i in range(1,11):
+    print(f"{n}*{i}={n*i}")
+"""
+# Task 4 – Sum of Numbers (1 to n)
+'''
+n = int(input("Enter a numbe :"))
+i = 1
+sum = 0
+while i <= n:
+    sum +=i
+    i += 1
+print(sum)
+'''
+# Task 5 – Factorial using Loop
+'''
+n = int(input("Enter a numbe :"))
+i = 1
+fact = 1
+while i <= n:
+    fact *=i
+    i += 1
+print(fact)
+'''
+# Task 6 – Break Statement
+"""
+for i in range(1,11):
+    if i == 5:
+        break
+    print(i)
+"""
+# Task 7 – Continue Statement
+"""
+for i in range(1,11):
+    if i == 3:
+        continue
+    print(i)
+"""
+# Task 8 – Pattern Printing
+'''
+n = int(input("Enter a num :"))
+for i in range(1,n+1):
+    print("*"*i,end=" ")
+    print(" ")
+'''
+# Task 9 – Add Two Numbers
+'''
+def total(a,b):
+    add = a+b
+    print(add)
+a = 10
+b = 20
+total(a,b)
+'''
+# Task 10 – Prime Checker
